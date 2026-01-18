e96bda04bc11d9ddffc884d237ab093fd51475bb
islamalama22 <islamalama22@gmail.com>
2026-01-19 00:35:54 +0200

    Login Without Vlaidation 19 jun

--- websit/src/pages/login/Login.jsx
+++ websit/src/pages/login/Login.jsx
@@ -16,10 +16,7 @@ function Login() {
   const loginForm = async (values) => {
     console.log(values);
     try {
-      const response = await axios.post(
-        `https://knowledgeshop.runasp.net/api/Auth/Account/login`,
-        values
-      );
+      const response = await axios.post(`https://knowledgeshop.runasp.net/api/Auth/Account/login`, values);
       if (response.status === 200) {
         localStorage.setItem("token", response.data.accessToken);
       }
@@ -33,29 +30,10 @@ function Login() {
     <Box className="Login-form">
       <Typography variant="h3"> Login Page</Typography>
 
-      <Box
-        onSubmit={handleSubmit(loginForm)}
-        component={"form"}
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          gap: 3,
-          mt: 5,
-          alignItems: "flex-start",
-        }}
-      >
-        <TextField
-          {...register("email")}
-          label="User  Email"
-          fullWidth
-          variant="outlined"
-        />
-        <TextField
-          {...register("password")}
-          label="Password"
-          fullWidth
-          variant="outlined"
-        />
+      <Box  onSubmit={handleSubmit(loginForm)}  component={"form"}
+        sx={{ display: "flex",   flexDirection: "column",   gap: 3,   mt: 5,  alignItems: "flex-start"  }}  >
+        <TextField  {...register("email")}  label="User  Email"  fullWidth  variant="outlined" />
+        <TextField   {...register("password")}  label="Password"   fullWidth  variant="outlined"   />
        <Box  display={'flex'}  gap={2}>
         <Button variant="contained" type="submit"> Login </Button>
         <Link  sx={{ border:1, padding:1, textTransform:'capitalize' }} component={RouterLink} to='/SendCode' color='inherit'  underline='none' > Resend  password</Link>
