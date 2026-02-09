3ef983320808b5aa5f09c4a3572d554d3c94ff97
islamalama22 <islamalama22@gmail.com>
2026-02-10 01:52:11 +0200

    dispaly orders in  profile

--- websit/src/pages/profile/ProfileOrders.jsx
+++ websit/src/pages/profile/ProfileOrders.jsx
@@ -1,13 +1,53 @@
 import React from 'react'
 import useProfile from '../../hooks/useProfile'
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 
 export default function ProfileOrders() {
     const {data ,isError,isLoading}=useProfile();
+    const orders=data?.orders;
+    console.log('Orders:',orders);
    
 
-  return (
-  <Typography>  profile orders  </Typography>
-//  { // data.map}
+  return ( 
+    <Box >
+    <Typography variant='h3'> orders</Typography>
+
+     <TableContainer >
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell align="right">id</TableCell>
+            <TableCell align="right">paid $</TableCell>
+            <TableCell align="right">paymentStatus</TableCell>
+            <TableCell align="right">status</TableCell>
+            <TableCell align="right">orderDate</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        {orders.map((order)=>(
+            <TableRow key={order.id} >
+              <TableCell align="right">{order.id}</TableCell>
+              <TableCell align="right">{order.amountPaid}</TableCell>
+               <TableCell align="right">{order.paymentStatus}</TableCell>
+              <TableCell align="right">{order.status}</TableCell>
+              <TableCell align="right">{order.orderDate}</TableCell>
+            </TableRow>
+        ))}
+           
+        </TableBody>
+      </Table>
+    </TableContainer>
+
+    <Typography variant='h4'> all  orders  count is:{orders.length}</Typography>
+    </Box>
+
   )
 }
